5d92e8e53754535c24fd48bb87a9408ea3aca2a9
alvinlubetkin <alvinlubetkin@system76-pc.localdomain>
2020-04-11 00:29:04 -0400

    just made a button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [mike, setMike] = useState("not ran");
+  console.log(mike);
+  const setRan = () => {
+    if (mike == "ran") {
+      setMike(`not Ran`);
+    } else {
+      setMike("ran");
+    }
+    console.log(mike);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Hello World</h1>
+      <button onClick={() => setRan()}>Make Michael Ran</button>
+      <div>is michael ran? => {mike}</div>
     </div>
   );
 }
